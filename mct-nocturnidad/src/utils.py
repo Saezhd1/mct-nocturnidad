f69dbfd6ab62cb1e544f7105299e4abb37d194c7
Saezhd1 <saezhd1@gmail.com>
2025-11-27 12:57:37 +0000

    Update utils.py

--- mct-nocturnidad/src/utils.py
+++ mct-nocturnidad/src/utils.py
@@ -1,9 +1,19 @@
-from weasyprint import HTML, CSS
+import os
 from flask import render_template
-import io, os
+from weasyprint import HTML, CSS
 
-def render_pdf(template_name, context):
+def render_pdf(template_name, context, base_url):
+    """
+    Renderiza un PDF a partir de una plantilla HTML y aplica estilos desde static/styles.css.
+    """
+    # Generar HTML con Jinja2
     html_str = render_template(template_name, **context)
-    base_url = os.path.dirname(os.path.abspath(__file__)) + "/../"
-    pdf = HTML(string=html_str, base_url=base_url).write_pdf(stylesheets=[CSS(string=open(os.path.join(base_url, "templates/_partials.css")).read())])
+
+    # Ruta al CSS real en static/
+    css_path = os.path.join(base_url, "static/styles.css")
+
+    # Generar PDF con WeasyPrint
+    pdf = HTML(string=html_str, base_url=base_url).write_pdf(
+        stylesheets=[CSS(filename=css_path)]
+    )
     return pdf
